4326f3bdf95582d4e4ac6b7fdbff2be151957bd9
nagendrasisodiya <3n3singh@gmail.com>
2026-03-06 22:11:09 +0530

    day-12 both task done

---
+++ Playwright/tests/Day-12-task1.spec.ts
@@ -0,0 +1,13 @@
+import {test} from "@playwright/test"
+
+test("olympic-task", async({page})=>{
+  await page.goto("https://www.olympics.com/en/")
+  await page.locator('//div[@class="sc-780ccd9c-2 ejyPot"]/descendant::section[@class="sc-fe434144-4 fCOCaX"]').click()
+  await page.locator('//div[@class="input-group"]/input[@type="search"]').fill("Emma McKeon")
+  await page.locator('//div[@class="input-group"]/button[@type="button"]').click()
+  await page.locator('//div[@class="search--results pb-5"]/descendant::a[@class="article--wrapper"]').click()
+  let g_medal=await page.locator('//div[@class="sc-ce26874d-10 fOoPCV"]/descendant::span[@class="Medal-styles__Medal-sc-645148e1-1 jFzKKI"]/span[@class="OcsText-styles__StyledText-sc-bf256156-0 cjPVFu text--sm-body"]').innerText()
+  console.log(g_medal)
+  let time=new Date().getTime()
+  await page.screenshot({path:`screenshot/day12-task1-sc-${time}.png`})
+})
