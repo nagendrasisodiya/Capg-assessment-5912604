c0d6b9273d4696afaccb67f721fb8b0de67a021a
nagendrasisodiya <3n3singh@gmail.com>
2026-03-16 21:28:26 +0530

    day-20 flipkart pom task done

---
+++ Playwright/tests/Day-20-task1.spec.ts
@@ -0,0 +1,9 @@
+import {test} from "@playwright/test";
+import flipkart from "../POM-Files/Day-20-task1.page";
+
+test("task1", async({browser})=>{
+    let context=await browser.newContext()
+    let page=await context.newPage()
+    let obj=new flipkart(page)
+    await obj.func("https://www.flipkart.com/")
+})
