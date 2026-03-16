8440393fe9319a7d458b2bdc6b4320cb62f29cf8
nagendrasisodiya <3n3singh@gmail.com>
2026-03-16 21:34:14 +0530

    day-20 task-1(with POM and JSON data)

--- Playwright/tests/Day-20-task1.spec.ts
+++ Playwright/tests/Day-20-task1.spec.ts
@@ -1,9 +1,15 @@
 import {test} from "@playwright/test";
 import flipkart from "../POM-Files/Day-20-task1.page";
+import * as fs from "node:fs";
+import path = require("node:path");
 
+
+let json_data=fs.readFileSync(path.join(__dirname, '../testing-data/Day-20-task1.json'))
+// @ts-ignore
+let data = JSON.parse(json_data)
 test("task1", async({browser})=>{
     let context=await browser.newContext()
     let page=await context.newPage()
     let obj=new flipkart(page)
-    await obj.func("https://www.flipkart.com/")
+    await obj.func(data.url)
 })
